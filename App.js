848921777d43f14b22c0f929534db8bf154f0484
Kenny Hu <kenny.hu@radar.com>
2024-04-18 10:23:01 -0400

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Radar from "react-native-radar";
+// this line should be called from native, putting it here just for simplicity
+Radar.initialize("prj_test_pk__");
+Radar.setUserId("test-user-id");
 
+async function startTracking() {
+  const foregroundStatus = await Radar.requestPermissions(false);
+
+  // request for background location permission
+  if (foregroundStatus === "GRANTED_FOREGROUND") {
+    Radar.trackOnce()
+    await Radar.requestPermissions(true);
+  }
+
+  // if background location permission was granted, register user for radar and start tracking
+  const status = await Radar.getPermissionsStatus();
+
+  if (status === "GRANTED_BACKGROUND") {
+    Radar.setUserId(user.id);
+    Radar.startTrackingResponsive();
+  }
+}
 export default function App() {
+  useEffect(() => {
+    startTracking();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
